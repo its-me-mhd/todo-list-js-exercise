cb24e02a1360413fc762d3b23af12b102a5bef7f
it's me <mohamedabdirisaaq57@gmail.com>
2024-10-27 20:34:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// Array to keep track of each task as an object
+const tasks = [];
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
+// Create a new task by adding to the tasks array
 function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+  const task = {
+    title: title,
+    complete: false
+  };
+  tasks.push(task);
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+// Mark a task as complete by setting the task's `complete` property to `true`
 function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
+  tasks[taskIndex].complete = true;
 }
 
-// Print the state of a task to the console in a nice readable way
+// Print the state of a task to the console in a readable way
 function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+  const task = tasks[taskIndex];
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
